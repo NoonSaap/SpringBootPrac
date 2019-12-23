932eb5a3abfea2dcf98d4810f446854676a58197
Lee-jun-hee <wns8432@naver.com>
2019-12-23 22:19:26 +0900

    JPAAuditing 테스트코드 추가

--- src/test/java/com/juni/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/juni/springboot/domain/posts/PostsRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,4 +43,22 @@ public class PostsRepositoryTest {
         assertThat(posts.getContent()).isEqualTo(content);
     }
 
+    @Test
+    public void BaseTimeEntity(){
+        LocalDateTime now = LocalDateTime.of(2019, 12,23,0,0,0);
+        postsRepository.save(Posts.builder()
+        .title("title")
+        .content("content")
+        .author("author")
+        .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+        Posts posts = postsList.get(0);
+
+        System.out.println("createDate" + posts.getCreatedDate());
+        System.out.println("modifiedDate" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
 }
