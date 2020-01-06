8ab0a26a33302777e51b11959c67245b83e962a6
Lee-jun-hee <wns8432@naver.com>
2020-01-06 21:18:49 +0900

    조회 기능 추가

--- src/main/java/com/juni/springboot/service/PostsService.java
+++ src/main/java/com/juni/springboot/service/PostsService.java
@@ -2,11 +2,15 @@ package com.juni.springboot.service;
 
 import com.juni.springboot.domain.posts.Posts;
 import com.juni.springboot.domain.posts.PostsRepository;
+import com.juni.springboot.web.dto.PostsListResponseDto;
 import com.juni.springboot.web.dto.PostsResponseDto;
 import com.juni.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -15,13 +19,13 @@ public class PostsService {
     private final PostsRepository postsRepository;
 
     @Transactional
-    public Long save(PostsSaveRequestDto requestDto){
+    public Long save(PostsSaveRequestDto requestDto) {
 
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
     @Transactional
-    public Long update(Long id, PostsSaveRequestDto requestDto){
+    public Long update(Long id, PostsSaveRequestDto requestDto) {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
@@ -29,9 +33,15 @@ public class PostsService {
         return id;
     }
 
-    public PostsResponseDto findById(Long id){
+    public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
     }
- }
+
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
